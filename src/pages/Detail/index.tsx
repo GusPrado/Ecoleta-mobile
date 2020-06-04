23d735c3d66caf6c4440265a9075ef8019f4b30a
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2020-06-04 18:58:23 -0300

    Added email/whatsapp feature

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
-import { View, Text, StyleSheet, Image, TouchableOpacity, SafeAreaView } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, SafeAreaView, Linking } from 'react-native'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler'
 import api from '../../services/api'
+import * as MailComposer from 'expo-mail-composer';
 
 interface Params {
   point_id: number;
@@ -41,6 +42,18 @@ const [data, setData] = useState<Data>({} as Data)
     navigation.goBack()
   }
 
+  function handleComposeMail() {
+      MailComposer.composeAsync({
+        subject: 'Interesse na coleta de resíduos',
+        recipients: [data.point.email]
+      })
+    
+  }
+
+  function handleWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse na coleta de resíduos`)
+  }
+
   if (!data.point) {
     return null
   }
@@ -62,12 +75,12 @@ const [data, setData] = useState<Data>({} as Data)
             <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
           </View>
           <View style={styles.footer}>
-            <RectButton style={styles.button} onPress={() => {}}>
+            <RectButton style={styles.button} onPress={handleWhatsapp}>
               <FontAwesome name="whatsapp" size={20} color="#FFF"/>
               <Text style={styles.buttonText}>Whatsapp</Text>
             </RectButton>
 
-            <RectButton style={styles.button} onPress={() => {}}>
+            <RectButton style={styles.button} onPress={handleComposeMail}>
               <Icon name="mail" size={20} color="#FFF"/>
               <Text style={styles.buttonText}>E-mail</Text>
             </RectButton>
@@ -129,7 +142,6 @@ const styles = StyleSheet.create({
     borderColor: '#999',
     paddingVertical: 20,
     paddingHorizontal: 8,
-    paddingBottom: 0,
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
